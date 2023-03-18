7cc61b74200c4588e32cb34f14a3697836f28407
Nico Pom <niconp7@yahoo.fr>
2023-03-18 17:16:52 +0100

    init

---
+++ main.js
@@ -0,0 +1,37 @@
+const cards = document.querySelectorAll(".card");
+
+document.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  cards.forEach((card) => {
+    const cardRect = card.getBoundingClientRect();
+    const cardCenterX = cardRect.left + cardRect.width / 2;
+    const cardCenterY = cardRect.top + cardRect.height / 2;
+    const intensity = 9;
+    const deltaX = mouseX - cardCenterX;
+    const deltaY = mouseY - cardCenterY;
+
+    /* Calculating the angle of the mouse relative to the center of the card. */
+    const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+    const angleX = (-deltaY / distance) * intensity;
+    const angleY = (deltaX / distance) * intensity;
+
+    if (card.classList.contains("hovered")) {
+      card.style.transform = `translateY(-5%) perspective(2500px) rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+    } else {
+      card.style.transform = `perspective(2500px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+    }
+  });
+});
+
+cards.forEach((card) => {
+  card.addEventListener("mouseover", () => {
+    card.classList.add("hovered");
+  });
+
+  card.addEventListener("mouseout", () => {
+    card.classList.remove("hovered");
+  });
+});
